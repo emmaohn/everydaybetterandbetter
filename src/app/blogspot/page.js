a4c398b785a235e5b7b3b1fb7f0d38951bfaa883
emmaohn <joh20123@byui.edu>
2024-03-28 14:12:24 -0600

    lockfile updates

--- src/app/blogspot/page.js
+++ src/app/blogspot/page.js
@@ -10,22 +10,27 @@ export default async function BlogSpot() {
 
   return (
     <div className='blogspot'>
-      {blogs.map(blog => {
-        let newDate = new DateTime(blog.blog_created);
-        return (<BlogCard 
-          key={blog}
-          imgPath={blog.blog_thumbnail}
-          title={blog.blog_title}
-          created={newDate.toLocaleString(DateTime.DATE_MED)}
-          content={blog.blog_content}
-        />)
-        })
-      }
-      <div className='blogspot-keywords'>
-        {keywords.map(keyword => {
-          return (<h3 key={keyword}>{keyword.keyword}</h3>)
-          })
-        }
+      <i id="blogspot-filter" className="blogspot-filter fa-solid fa-sliders"></i>
+      <div className="blogspot-grid">
+        <div className='blogspot-blogs'>
+          {blogs.map(blog => {
+            let newDate = new DateTime(blog.blog_created);
+            return (<BlogCard 
+              key={blog}
+              imgPath={blog.blog_thumbnail}
+              title={blog.blog_title}
+              created={newDate.toLocaleString(DateTime.DATE_MED)}
+              content={blog.blog_content}
+            />)
+            })
+          }
+        </div>
+        <div className='blogspot-keywords'>
+          {keywords.map(keyword => {
+            return (<h3 key={keyword}>{keyword.keyword}</h3>)
+            })
+          }
+        </div>
       </div>
     </div>
   );
